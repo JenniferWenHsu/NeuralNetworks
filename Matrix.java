3fd1a9e3892eab8d91360553463ea1d096b74859
JenniferWenHsu <jwhsu@berkeley.edu>
2017-06-11 19:22:02 -0700

    add general methods to Matrix class and implement feedForward, update_mini_batch, sigmoid, and sigmoid_prime

--- Matrix.java
+++ Matrix.java
@@ -1,12 +1,108 @@
+/**
+* Standard Matrix class. 
+* Created by Jennifer Hsu on June 11 2017
+*/
 import java.util.Random; 
 public class Matrix{
-	// Returns a random m-by-n matrix with values between 0 and 1 
-	public static double[][] randomMatrix(int m, int n){
-		double[][] a = new double[m][n]; 
+
+	private int M; // Number of rows
+	private int N; // Number of columns 
+	private double[][] data; // M-by-N array 
+
+	// Create M-by-N matrix of 0's 
+	public Matrix(int M, int N){
+		this.M = M; 
+		this.N = N; 
+		data = new double[M][N]; 
+	}
+
+	// Create matrix based on 2d array 
+	public Matrix(double[][] data){
+		M = data.length; 
+		N = data[0].length; 
+		this.data = new double[M][N]; 
+		for(int i = 0; i<M ;i++){
+			for(int j = 0; j< N; j++){
+				this.data[i][j] = data[i][j]; 
+			}
+		}
+	}
+	// Create and return a random M-by-N matrix with values between 0 and 1 
+	public static Matrix randomMatrix(int m, int n){
 		Random rand = new Random(); 
-		for(int i=0; i<m; i++)
-			for(int j=0; j<n; j++)
-				a[i][j] = rand.nextGaussian(); 
-		return a; 
+		Matrix result = new Matrix(m, n); 
+		for(int i=0; i<m ;i++){
+			for(int j=0; j<n; j++){
+				result.data[i][j] = rand.nextGaussian(); 
+			}
+		}
+		return result; 
+	}
+ 
+ 	// Return C = A + B
+ 	public Matrix plus(Matrix B){
+ 		Matrix A = this; 
+ 		if (B.M != A.M || B.N != A.N) throw new RuntimeException("Illegal matrix dimensions"); 
+ 		Matrix C = new Matrix(M, N); 
+ 		for(int i=0; i<M; i++){
+ 			for(int j=0; j<N; j++){
+ 				C.data[i][j] = A.data[i][j] + B.data[i][j]; 
+ 			}
+ 		}
+ 	}
+
+ 	// Return C = A - B
+ 	public Matrix minus(Matrix B){
+ 		Matrix A = this; 
+ 		if (B.M != A.M || B.N != A.N) throw new RuntimeException("Illegal matrix dimensions"); 
+ 		Matrix C = new Matrix(M, N); 
+ 		for(int i=0; i<M; i++){
+ 			for(int j=0; j<N; j++){
+ 				C.data[i][j] = A.data[i][j] - B.data[i][j]; 
+ 			}
+ 		}
+ 	}
+	// Return C = A*B
+	public Matrix times(Matrix B){
+		Matrix A = this; 
+		if (A.N != B.M) throw new RuntimeException("Illegal matrix dimensions"); 
+		Matrix C = new Matrix(M,N); 
+		for(int i=0; i<M; i++){
+			for(int j=0; j<N; j++){
+				for(int k=0; k<A.N; k++)
+					C.data[i][j] += (A.data[i][k] + B.data[k][j]); 
+			}
+		}
+	}
+
+	// Return C = A*x, with x being a double. 
+	public Matrix times(double x){
+		double[][] temp = new double[N][1]; 
+		for(int i=0; i<N; i++){
+			temp[i][0] = x; 
+		}
+		Matrix C = new Matrix(temp); 
+		return C; 
+	}
+	/*
+	* Modifiers and Accessors
+	*/
+	// Accessor 
+	public double getValue(int x, int y){
+		if(x >= M || x < 0 || y >= N || y < 0)
+			throw new ArrayIndexOutOfBoundsException("Array Index out of bounds"); 
+		return this.data[x][y]; 
+	}
+
+	public double getM(){
+		return this.M; 
+	}
+
+	public double getN(){
+		return this.N; 
+	}
+
+	public double[][] get2dArray(){
+		return this.data; 
 	}
 }
--- Network.java
+++ Network.java
@@ -1,19 +1,25 @@
-// network.javainclud
-
-import java.util.ArrayList; 
-
 /**
+* A module that implements a feed-forward neural networks using sigmoid neurons
+* and stochastic gradient descent learning algorithm. 
 * Created by Jennifer Hsu on June 10 2017
 **/
+
+
+//-------------------------------------------
+// Standard library
+import java.util.ArrayList; 
+import java.lang.Math; 
+
 public class Network{
-	/**
-	* The int array SIZES contain the number of neurons in the respective
-	* the respective layers of the network. 
+
+	/*
+	 *The int array SIZES contain the number of neurons in the respective 
+	 *the respective layers of the network. 
 	*/
 	public Network(int[] sizes){
 		int num_layers = sizes.length; 
-		ArrayList<double[][]> biases = new ArrayList<double[][]>(); 
-		ArrayList<double[][]> weights = new ArrayList<double[][]>(); 
+		ArrayList<Matrix> biases = new ArrayList<Matrix>(); 
+		ArrayList<Matrix> weights = new ArrayList<Matrix>(); 
 
 		for(int i=0; i<sizes.length; i++){
 			biases.add(Matrix.randomMatrix(i, 1));
@@ -34,9 +40,126 @@ public class Network{
 		}
 	}
 
-	/**
-	* Return the output of the network if A is the input
+	/*Return the output of the network if A is the input*/
+	public static Matrix feedForward(Matrix A){
+		if(biases.size() != weights.size()) 
+			throw new RuntimeException("Size of biases does not match size of weights"); 
+		for(int i=0; i<biases.size(); i++){
+			A = sigmoid(biases.get(i).times(A) + weights.get(i)); 
+		}
+
+		return A; 
+	}
+
+	/*
+	 * Train the neural network using the mini-batch stochastic gradient descent. 
+	 * TRAINING_DATA is an ArrayList of tuples (x, y) representing the training
+	 * inputs and the desired outputs. x is a n-by-1 matrix with n being the dimension
+	 * of the input vector. 
+	 *
+	 * If TEST_DATA is provided, then the network will be evaluated against the test
+	 * data after each epoch, and partial progress will be printed out. 
+	 * May be deleted if needs to train at a faster speed.
+	 * There are no differences in performance.  
+	 */
+	public static void stochastic_gradient_descent(
+		ArrayList<Data> trainingData, int epochs, int miniBatchSize,
+		double eta, ArrayList<Data> testData = null){
+		// TODO
+
+	}
+
+	public static void update_mini_batch(ArrayList<Data> miniBatch, double eta){
+		ArrayList<Matrix> nabla_b = new ArrayList<Matrix>(); 
+		for(Matrix bias : biases){
+			Matrix zeros_b = new Matrix(bias.getM(), bias.getN());
+			nabla_b.add(zeros_b);   
+		}
+
+		ArrayList<Matrix> nabla_w = new ArrayList<Matrix>(); 
+		for(Matrix bias : biases){
+			Matrix zeros_w = new Matrix(bias.getM(), bias.getN()); 
+			nabla_w.add(zeros_w); 
+		}
+
+		// TODO: Need to update biases and weights. 
+		for(Data batch : miniBatch){
+			ArrayList<ArrayList<Matrix>> delta = backPropagation(batch); 
+			ArrayList<Matrix> delta_nabla_b = delta.get(0);
+			ArrayList<Matrix> delta_nabla_w = delta.get(1);
+
+			for(int i=0; i<delta_nabla_b.size(); i++){
+				 nabla_b.set(i, delta_nabla_b.plus(nabla_b)); 
+			}
+
+			for(int i=0; i<delta_nabla_w.size(); i++){
+				nabla_w.set(i, delta_nabla_w.plus(nabla_w)); 
+			}
+		}
+
+		for(int i=0; i<weights.size();i++){
+			Matrix w = weights.get(i); 
+			Matrix nw = nabla_w.get(i); 
+
+			weights.set(i, w.minus(nw.times(eta/miniBatch.size()))); 
+		}
+
+		for(int i=0; i<biases.size(); i++){
+			Matrix b = biases.get(i); 
+			Matrix nb = nabla_b.get(i); 
+
+			biases.set(i, b.minus(nb.times(eta/miniBatch.size()))); 
+		}
+
+	}
+
+	/*
+	 * Return an ArrayList 
+	 */
+	public static ArrayList<Matrix> backPropagation(Data data){
+		ArrayList<Matrix> nabla_b = new ArrayList<Matrix>(); 
+		for(Matrix bias : biases){
+			Matrix zeros_b = new Matrix(bias.getM(), bias.getN());
+			nabla_b.add(zeros_b);   
+		}
+
+		ArrayList<Matrix> nabla_w = new ArrayList<Matrix>(); 
+		for(Matrix bias : biases){
+			Matrix zeros_w = new Matrix(bias.getM(), bias.getN()); 
+			nabla_w.add(zeros_w); 
+		}
+
+		// TODO : The rest of the method
+		ArrayList<ArrayList<Matrix>> result = new ArrayList<ArrayList<Matrix>>(); 
+		return result; 
+
+	}
+
+	/*
+	* Miscellaneous functions 
 	*/
 
+	// The sigmoid function 
+	public Matrix sigmoid(Matrix z){
+		double[][] 2darray = new double[z.getM()][1]; 
+		for(int i=0; i<z.getM(); i++){
+			2darray[i][0] = 1.0/(1.0 + Math.exp(-1*z.getValue(i, 0));
+		}
+
+		Matrix result = new Matrix(2darray); 
+		return result; 
+	}
+
+	// Derivative of the sigmoid function 
+	public Matrix sigmoid_prime(Matrix z){
+		Matrix sigmoid = sigmoid(z); 
+		double[][] 2darray = new double[z.getM()][1];
+		double[][] 2dsigmoid = sigmoid.get2dArray();  
+		for(int i=0; i<z.getM(); i++){
+			2darray[i][0] = 2dsigmoid[i][0]*(1-2dsigmoid[i][0]); 
+		}
+		Matrix result = new Matrix(2darray); 
+		return result;
+	}
 
 }
